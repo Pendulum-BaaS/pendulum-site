740f256a4fc5dbc44618d0366d5954128788e8f1
Sean Lacey <seanlacey2164@gmail.com>
2025-08-25 20:01:15 -0400

    feat: add visuals and reflect latest content
    All case study pages are .astro files now instead of markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  integrations: [mdx()]
+});
